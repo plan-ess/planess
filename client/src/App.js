d3827acf471802850cf987a844b698d1121b9b55
or <prietopazjosue@gmail.com>
2021-07-22 14:13:22 +0200

    my changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+//import Projects from './components/Projects';
+//import ProjectDetails from './components/ProjectDetails';
+//import Navbar from './components/Navbar';
+import Signup from './components/Signup';
+//import ProtectedRoute from './components/ProtectedRoute';
+import Login from './components/Login';
+import { Route, Redirect } from 'react-router-dom';
+import WelcomePage from './components/welcomePage'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    user: this.props.user
+  }
+
+  setUser = user => {
+    this.setState({
+      user: user
+    })
+  }
+
+  render() {
+    return (
+      <div className="App" >
+
+        <WelcomePage user={this.state.user} setUser={this.setUser}/>
+
+
+        {/* <Route
+          exact path='/projects'
+          component={Projects}
+        /> */}
+        {/* This route is now protected */}
+        {/* <Route
+          exact path='/projects'
+          // here we also use a render prop
+          render={props => {
+            if (this.state.user) return <Projects {...props} />
+            else return <Redirect to='/' />
+          }}
+        /> */}
+        {/* <Route
+          exact path='/signup'
+          // component={Signup}
+          render={props => <Signup setUser={this.setUser} {...props} />}
+        />
+        <Route
+          exact path='/login'
+          render={props => <Login setUser={this.setUser} {...props} />} */}
+        {/* /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/Signup.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react'
+import { signup } from '../services/auth';
+
+export default class Signup extends Component {
+
+	state = {
+		email: '',
+        name: '',
+		password: '',
+		message: ''
+	}
+
+	handleChange = e => {
+		const { name, value } = e.target;
+		this.setState({
+			[name]: value
+		})
+	}
+
+	handleSubmit = e => {
+		e.preventDefault();
+		const { name, email, password } = this.state;
+		signup(name, email, password)
+			.then(response => {
+				if (response.message) {
+					this.setState({
+						message: response.message,
+						name: '',
+                        email: '',
+						password: ''
+					})
+				} else {
+					// user is correctly signed up in the backend
+					// -> we want to add the user also in the state of App.js
+					this.props.setUser(response);
+					// redirect to /projects
+					this.props.history.push('/projects');
+				}
+			})
+	}
+
+	render() {
+		return (
+			<>
+				<h2>Signup</h2>
+				<form onSubmit={this.handleSubmit}>
+					<label htmlFor="name">name: </label>
+					<input
+						type="text"
+						name="name"
+						value={this.state.name}
+						onChange={this.handleChange}
+					/>
+                    <label htmlFor="email">email: </label>
+					<input
+						type="text"
+						name="email"
+						value={this.state.email}
+						onChange={this.handleChange}
+					/>
+					<label htmlFor="password">Password: </label>
+					<input
+						type="password"
+						name="password"
+						value={this.state.password}
+						onChange={this.handleChange}
+					/>
+					<button type="submit">Signup</button>
+					{this.state.message && (
+						<h3>{this.state.message}</h3>
+					)}
+				</form>
+			</>
+		)
+
+	}
+}
